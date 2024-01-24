f9d225768dbccf097f2c04f6a8bd49a41ca1063b
andremazzali <mazzali.andre@gmail.com>
2024-01-24 11:28:23 -0300

    final aula 04

---
+++ src/main/kotlin/me/dio/credit/application/system/service/CreditService.kt
@@ -0,0 +1,10 @@
+package me.dio.credit.application.system.service
+
+import me.dio.credit.application.system.entity.Credit
+import java.util.UUID
+
+interface ICreditService {
+    fun save(credit: Credit): Credit
+    fun findAllbyCustomer(customerId: Long):List<Credit>
+    fun findByCreditCode(customerId: Long, creditcode: UUID): Credit
+}
--- src/main/kotlin/me/dio/credit/application/system/service/ICustomerService.kt
+++
@@ -1,4 +0,0 @@
-package me.dio.credit.application.system.service
-
-interface ICustomerService {
-}
